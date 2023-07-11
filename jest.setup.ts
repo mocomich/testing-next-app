bacb1af69f90993840dc67eb3ba8f5ba839fec03
mk <mk.eng1119@gmail.com>
2023-07-11 21:34:23 +0900

    add settings for react-router-mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+
+// MEMO: @storybook/testing-react で globalSetup している場合は不要
+jest.mock('next/router', () => require('next-router-mock'));
+jest.mock('next/dist/client/router', () => require('next-router-mock'));
+// https://github.com/scottrippey/next-router-mock/issues/58
+jest.mock('next/dist/shared/lib/router-context', () => {
+  const { createContext } = require('react');
+  const { memoryRouter } = require('next-router-mock');
+  const RouterContext = createContext(memoryRouter);
+  return { RouterContext };
+});
