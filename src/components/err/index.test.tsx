3ebe6f5195bc9356ba95f3f16a05aa17889f09b9
mk <mk.eng1119@gmail.com>
2023-07-13 18:10:56 +0900

    format

--- src/components/err/index.test.tsx
+++ src/components/err/index.test.tsx
@@ -7,7 +7,10 @@ const ErrorArray: Err[] = Object.entries(errors).map(([key, value]) => ({
   message: value['message'],
 }));
 
-test.each(ErrorArray)('Status:$statusが渡ってきた場合、$messageが日本化され表示される', ({ message, status }) => {
-  render(<Error message={message} status={status} />);
-  expect(screen.getByText(JPMessages[status]['message'])).toBeInTheDocument();
-});
+test.each(ErrorArray)(
+  'Status:$statusが渡ってきた場合、$messageが日本化され表示される',
+  ({ message, status }) => {
+    render(<Error message={message} status={status} />);
+    expect(screen.getByText(JPMessages[status]['message'])).toBeInTheDocument();
+  },
+);
