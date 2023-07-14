07e3e02b1d57a8e1ab75826dd28abe93e396719b
mk <mk.eng1119@gmail.com>
2023-07-14 16:01:52 +0900

    format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         </Head>
         <Component {...pageProps} />
       </>,
-      pageProps
-    )
+      pageProps,
+    ),
   );
 }
