6e097ecceddaf691da7d5abfa4a5d7a83c6549a3
mk <mk.eng1119@gmail.com>
2023-07-16 22:39:24 +0900

    for using svg by inline

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
 
-module.exports = nextConfig;
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
+  },
+  images: {
+    disableStaticImages: true,
+  },
+}
+
+module.exports = nextConfig
