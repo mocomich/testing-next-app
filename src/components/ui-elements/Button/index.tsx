75c9156c7efbaed614bf7fb50b4ee734ecd21007
mk <mk.eng1119@gmail.com>
2023-07-20 00:48:15 +0900

    upgrade button component

--- src/components/ui-elements/Button/index.tsx
+++ src/components/ui-elements/Button/index.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-text hover:bg-primary/80',
+        primary: 'bg-primary text-primary-text hover:bg-primary/80',
         secondary: 'bg-secondary text-secondary-text hover:bg-secondary/80',
         tertiary: 'bg-tertiary text-tertiary-text hover:bg-tertiary/80',
         error: 'bg-error text-error-text hover:bg-error/80',
@@ -16,36 +16,30 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
+        md: 'h-10 px-4 py-2',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: 'primary',
+      size: 'md',
     },
   },
 )
 
 export type ButtonProps = React.ComponentPropsWithRef<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    as?: React.ElementType
-  }
+  VariantProps<typeof buttonVariants>
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ as, className, variant, size, ...props }, ref) => {
-    const Component = as ? as : 'button'
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  function ButtonBase({ className, variant, size, ...props }, ref) {
     return (
-      <Component
-        className={combineStyles(buttonVariants({ variant, size, className }))}
-        ref={ref}
+      <button
         {...props}
+        ref={ref}
+        className={combineStyles(buttonVariants({ variant, size, className }))}
       />
     )
   },
 )
-Button.displayName = 'Button'
-
-export { Button, buttonVariants }
