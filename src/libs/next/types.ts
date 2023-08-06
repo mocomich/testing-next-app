46ccb407be3ef2ccf72e9b404a0b35438c62d61e
mk <mk.eng1119@gmail.com>
2023-08-06 17:30:38 +0900

    add ssr result type

--- src/libs/next/types.ts
+++ src/libs/next/types.ts
@@ -1,12 +1,17 @@
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-import type { ReactElement } from 'react';
+import type { NextPage } from 'next'
+import type { AppProps } from 'next/app'
+import type { ReactElement } from 'react'
+import { Err } from '../error'
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout: (page: ReactElement) => ReactElement;
-  getPageTitle: (page: ReactElement, pageProps: P) => ReactElement;
-};
+  getLayout: (page: ReactElement) => ReactElement
+  getPageTitle: (page: ReactElement, pageProps: P) => ReactElement
+}
 
 export type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
+  Component: NextPageWithLayout
+}
+
+export type SsrResult<T> = { data: T; err: null } | { data: null; err: Err }
+
+export type NextPageWithSsrResult<T> = NextPageWithLayout<SsrResult<T>>
