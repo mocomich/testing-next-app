b14665bc1cd630e976bd8213176c411803a872cf
mk <mk.eng1119@gmail.com>
2023-09-02 23:19:09 +0900

    add Markdown component

---
+++ src/components/ui-parts/Markdown/index.tsx
@@ -0,0 +1,31 @@
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { coldarkDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+export const Markdown = ({ body }: { body: string }) => {
+  return (
+    <ReactMarkdown
+      className="markdown"
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={coldarkDark as any}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          )
+        },
+      }}
+    >
+      {body}
+    </ReactMarkdown>
+  )
+}
