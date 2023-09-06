7d2ee2924e09a0a869ca89dad9fba30968bea436
mk <mk.eng1119@gmail.com>
2023-09-06 16:49:15 +0900

    fix type

--- src/components/ui-elements/Image/index.tsx
+++ src/components/ui-elements/Image/index.tsx
@@ -1,15 +1,13 @@
-import NextImage, { ImageProps as NextImageProps } from 'next/image'
+import NextImage, { ImageProps } from 'next/image'
 import { combineStyles } from '@/libs/utils/style'
 import * as React from 'react'
-
-type ImageProps = NextImageProps
 
 export const Image = ({ className, ...props }: ImageProps) => {
   return (
     <NextImage
       fill
-      {...props}
       className={combineStyles('!relative w-auto object-cover', className)}
+      {...props}
     />
   )
 }
