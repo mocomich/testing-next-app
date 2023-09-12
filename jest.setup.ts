7c57e641ee2c3ea27c22368707a9a8a84569b51e
mk <mk.eng1119@gmail.com>
2023-09-12 20:44:26 +0900

    chore jest-axe setting

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,12 +1,15 @@
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+import { toHaveNoViolations } from 'jest-axe'
+import 'jest-axe/extend-expect'
 
 // MEMO: @storybook/testing-react で globalSetup している場合は不要
-jest.mock('next/router', () => require('next-router-mock'));
-jest.mock('next/dist/client/router', () => require('next-router-mock'));
+jest.mock('next/router', () => require('next-router-mock'))
+jest.mock('next/dist/client/router', () => require('next-router-mock'))
 // https://github.com/scottrippey/next-router-mock/issues/58
 jest.mock('next/dist/shared/lib/router-context', () => {
-  const { createContext } = require('react');
-  const { memoryRouter } = require('next-router-mock');
-  const RouterContext = createContext(memoryRouter);
-  return { RouterContext };
-});
+  const { createContext } = require('react')
+  const { memoryRouter } = require('next-router-mock')
+  const RouterContext = createContext(memoryRouter)
+  return { RouterContext }
+})
+expect.extend(toHaveNoViolations)
