ed044f77f437934952896c19b08a141ec742c345
mk <mk.eng1119@gmail.com>
2023-09-15 16:41:17 +0900

    fix: config for analyze bundler

--- next.config.js
+++ next.config.js
@@ -18,9 +18,8 @@ const nextConfig = {
   },
 }
 
-const withBundleAnalyzer =
-  process.env.ANALYZE === 'true'
-    ? require('@next/bundle-analyzer')({ enabled: true })
-    : (config) => config
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = withBundleAnalyzer({ nextConfig })
+module.exports = withBundleAnalyzer({ ...nextConfig })
